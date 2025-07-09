559d86c04f7e8a7914ec36011cc9d275bee460a9
TlokotseSM <tlokotsemogudi@gmail.com>
2025-07-09 11:59:13 +0200

    Implemented posts.controller.ts to handle blog post operations

---
+++ src/posts/posts.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get, Post, Body, Param, UseGuards } from '@nestjs/common';
+import { PostsService } from './posts.service';
+import { CreatePostDto } from './dto/create-post.dto';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+
+@Controller('posts')
+export class PostsController {
+  constructor(private readonly postsService: PostsService) {}
+
+  @Get()
+  findAll() {
+    return this.postsService.findAll();
+  }
+
+  @Post()
+  @UseGuards(JwtAuthGuard)
+  create(@Body() createPostDto: CreatePostDto) {
+    return this.postsService.create(createPostDto);
+  }
+}
