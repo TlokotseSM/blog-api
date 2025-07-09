c8cc3bdc991950e8a74442921228f0c2189d9104
TlokotseSM <tlokotsemogudi@gmail.com>
2025-07-09 10:52:39 +0200

    Set Up MongoDB Connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (config: ConfigService) => ({
+        uri: config.get<string>('MONGODB_URI'), // Store in .env
+      }),
+      inject: [ConfigService],
+    }),
+  ],
 })
 export class AppModule {}
