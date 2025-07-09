ab44c289aa0b9bdef499b309c7cd4a650b82e9e3
TlokotseSM <tlokotsemogudi@gmail.com>
2025-07-09 11:49:31 +0200

    Initialized the auth module with necessary imports and providers

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { UsersModule } from '../users/users.module';
+import { AuthService } from './auth.service';
+import { JwtStrategy } from './strategies/jwt.strategy';
+
+@Module({
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
+  providers: [AuthService, JwtStrategy],
+})
+export class AuthModule {}
