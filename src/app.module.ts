07a3cc914d64ae566acd3f039fe5cbcaf7fe7eda
TlokotseSM <tlokotsemogudi@gmail.com>
2025-07-10 14:07:30 +0200

    Updated my AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module'; 
 
 @Module({
   imports: [
@@ -8,10 +9,11 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: async (config: ConfigService) => ({
-        uri: config.get<string>('MONGODB_URI'), // Store in .env
+        uri: config.get<string>('MONGODB_URI'),
       }),
       inject: [ConfigService],
     }),
+    AuthModule, // Added this line to register my auth routes
   ],
 })
 export class AppModule {}
